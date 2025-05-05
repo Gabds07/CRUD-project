9411c74f7c29cdda90323daffacda24a66c889fb
gabrielam12102007@gmail.com <gabrielam12102007@gmail.com>
2025-05-05 15:55:34 -0300

    Some fixes (again)

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -2,18 +2,16 @@ const User = require('../models/User.js');
 const validator = require('email-validator');
 
 module.exports = {
-  getUsersData(req, res) {
-    async function usersVerifier() {
-      try {
-        const getUsers = await User.find();
+  async getUsersData(req, res) {
+    try {
+      const getUsers = await User.find();
 
-        if (getUsers.length > 0) return res.status(201).json(getUsers);
-        if (getUsers.length === 0) return res.status(404).json({ error: 'No users found.' });
-      } catch {
-        res.status(501).json({ error: 'Error by trying to find users.' });
-      }
+      if (getUsers.length > 0) return res.status(201).json(getUsers);
+      if (getUsers.length === 0) return res.status(400).json({ error: 'No users found.' });
+    } catch (err) {
+      console.log(err);
+      return res.status(501).json({ error: 'Error by trying to find users.' });
     }
-    usersVerifier();
   },
 
   async getUsersById(req, res) {
@@ -22,28 +20,26 @@ module.exports = {
       const getUserById = await User.findById(_id);
 
       res.status(201).json(getUserById);
-    } catch {
-      res.status(501).json({ error: 'Error by trying to find user by Id.' });
+    } catch (err) {
+      console.log(err);
+      return res.status(501).json({ error: 'Error by trying to find user by Id.' });
     }
   },
 
-  postData(req, res) {
-    async function userDataVerifier() {
-      try {
-        const { name, email } = req.body;
-        let newUser = { name, email };
+  async postData(req, res) {
+    try {
+      const { name, email } = req.body;
+      let newUser = { name, email };
 
-        if (newUser.name.length < 3 || newUser.name.length > 20) return res.status(404).json({ error: 'The name must have between 3 and 20 caractheres' });
-        if (!validator.validate(newUser.email)) return res.status(404).json({ error: 'E-mail is invalid.' });
+      if (newUser.name.length < 3 || newUser.name.length > 20) return res.status(400).json({ error: 'The name must have between 3 and 20 caractheres' });
+      if (!validator.validate(newUser.email)) return res.status(400).json({ error: 'E-mail is invalid.' });
 
-        if (validator.validate(newUser.email) && newUser.name.length >= 3 || newUser.name.length <= 20) {
-          newUser = await User.create({ name, email });
-          return res.status(201).json(newUser);
-        }
-      } catch {
-        return res.status(501).json({ error: 'Error by trying to create a new user.' });
-      }
+      newUser = await User.create({ name, email });
+      return res.status(201).json(newUser);
+    } catch (err) {
+      console.log(err);
+      return res.status(501).json({ error: 'Error by trying to create a new user.' });
+
     }
-    userDataVerifier();
   }
 };
