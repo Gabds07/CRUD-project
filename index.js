df0f4b083ae164f582ee402b6e82e4600d91f0a7
gabrielam12102007@gmail.com <gabrielam12102007@gmail.com>
2025-05-05 02:18:54 -0300

    tiny change in index

--- index.js
+++ index.js
@@ -9,8 +9,6 @@ app.use(express.json());
 
 app.post('/user', UserController.userDataverifier);
 app.get('/user', UserController.usersVerifier);
-app.get('/', (req, res) => { res.send({ "status": "Status ON", }); });
-
 
 mongoose.connect(process.env.MONGODB_CONNECTION).then(() => {
   console.log('Connected to mongodb');
